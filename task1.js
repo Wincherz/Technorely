b3a13d91bd85b30d92adf4862205960ee63b65fc
WincherZ <wincher2016@gmail.com>
2020-08-30 18:09:40 +0300

    some fix

--- task1.js
+++ task1.js
@@ -1,15 +1,21 @@
 const func = (arr = []) => {
   const [a = 0, b = 0, c = 0] = arr;
+  const inParam = `Входящий параметр: ${a}, ${b}, ${c}`;
+  const expr = `${a}*x^2+${b}+${c}=0`;
 
   const discr = b**2 - 4 * a * c;
+
+  console.log(inParam);
+  console.log('Условие: ' + expr)
+  console.log('Дискриминант: ' + discr);
 
   if (discr < 0) {
     console.log('Нет корней');
   } else if (discr === 0) {
-    console.log(-b / 2 * a + '');
+    console.log('Корень: ' + (-b / 2 * a));
   } else if (discr > 0) {
-    console.log((-b + Math.sqrt(discr)) / 2 * a + '');
-    console.log((-b - Math.sqrt(discr)) / 2 * a + '');
+    console.log('Корень 1: ' + ((-b + Math.sqrt(discr)) / 2 * a));
+    console.log('Корень 2: ' + ((-b - Math.sqrt(discr)) / 2 * a));
   } else {
     console.log('Не верные данные');
   }
