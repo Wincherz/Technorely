2dcb47a371f3644e2258131ac318ec6452b1d053
WincherZ <wincher2016@gmail.com>
2020-08-31 16:48:30 +0300

    some changes

--- task2.js
+++ task2.js
@@ -1,34 +1,35 @@
 var myGlobal = 10;
-var oopsGlobal;
 
 const func1 = () => {
   oopsGlobal = 5;
 };
+
+func1()
 
 const func2 = () => {
   var output = '';
   if (typeof myGlobal !== undefined) {
     output = undefined;
     output = `myGlobal: ${myGlobal}`;
-    console.log(output)
+    console.log(output);
   }
   if (typeof oopsGlobal !== undefined) {
     output = undefined;
     output = `myGlobal: ${oopsGlobal}`;
-    console.log(output)
+    console.log(output);
   }
 }
 
-func2()
+func2();
 
 
 const myLocalScope = () => {
   'use strict'
-  var myVar = 'my local variable'
+  var myVar = 'my local variable';
 
-  console.log(`inside myLocalScope ${myVar}`)
+  console.log(`inside myLocalScope ${myVar}`);
 }
 
-myLocalScope()
+myLocalScope();
 
-console.log(`outside myLocalScope ${myVar}`)
+console.log(`outside myLocalScope ${myVar}`);
